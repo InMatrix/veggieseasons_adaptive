530005a725fb6def67fcb474a9f82d489ded3e98
Tao Dong <taodong@google.com>
2023-02-17 15:09:59 -0800

    Remove unnecessary comments

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -20,76 +20,69 @@ class SettingScreen extends StatelessWidget {
         ),
         SliverList(
           // Use a delegate to build items as they're scrolled on screen.
-          delegate: SliverChildListDelegate(
-              // // The builder function returns a ListTile with a title that
-              // // displays the index of the current item.
-              // (context, index) => ListTile(title: Text('Item #$index')),
-              // // Builds 1000 ListTiles
-              // childCount: 1000,
-              [
-                ListTile(
-                  leading: Icon(
-                    Icons.track_changes_outlined,
-                    size: 32.0,
-                  ),
-                  title: Text("Calorie Target"),
-                  trailing: Text("2000"),
-                  onTap: () => showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text('Set Calorie Target'),
-                      content: const Text('This page is under construction'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, 'Dismiss'),
-                          child: const Text('Dismiss'),
-                        ),
-                      ],
+          delegate: SliverChildListDelegate([
+            ListTile(
+              leading: Icon(
+                Icons.track_changes_outlined,
+                size: 32.0,
+              ),
+              title: Text("Calorie Target"),
+              trailing: Text("2000"),
+              onTap: () => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Set Calorie Target'),
+                  content: const Text('This page is under construction'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Dismiss'),
+                      child: const Text('Dismiss'),
                     ),
-                  ),
+                  ],
                 ),
-                ListTile(
-                  leading: Icon(
-                    Icons.favorite_outline_rounded,
-                    size: 32.0,
-                  ),
-                  title: Text("Preferred Categories"),
-                  subtitle: Text("What types of veggies you prefer!"),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                VeggieCategorySettingsScreen()));
-                  },
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.delete_outline_rounded,
-                    size: 32.0,
-                  ),
-                  title: Text("Restore Defaults"),
-                  // TODO: try https://pub.dev/packages/adaptive_dialog
-                  onTap: () => showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text('Are you sure?'),
-                      content: const Text(
-                          'Are you sure you want to reset the current settings?'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, 'Yes'),
-                          child: const Text('Yes'),
-                        ),
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, 'No'),
-                          child: const Text('No'),
-                        ),
-                      ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.favorite_outline_rounded,
+                size: 32.0,
+              ),
+              title: Text("Preferred Categories"),
+              subtitle: Text("What types of veggies you prefer!"),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => VeggieCategorySettingsScreen()));
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.delete_outline_rounded,
+                size: 32.0,
+              ),
+              title: Text("Restore Defaults"),
+              // TODO: try https://pub.dev/packages/adaptive_dialog
+              onTap: () => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Are you sure?'),
+                  content: const Text(
+                      'Are you sure you want to reset the current settings?'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Yes'),
+                      child: const Text('Yes'),
                     ),
-                  ),
-                )
-              ]),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'No'),
+                      child: const Text('No'),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ]),
         ),
       ],
     );
