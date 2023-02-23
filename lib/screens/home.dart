1b1b3c02ade1410277307817e91c1b095c8689b5
Tao Dong <taodong@google.com>
2023-02-23 13:34:07 -0800

    Implement the in-season filter

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:veggieseasons_adaptive/data/veggie.dart';
 
 import 'package:veggieseasons_adaptive/data/veggie_data.dart';
 import 'package:veggieseasons_adaptive/screens/favorites.dart';
@@ -18,6 +19,44 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int currentPageIndex = 0;
 
+  Season? _getSeasonForDate(DateTime date) {
+    // Technically the start and end dates of seasons can vary by a day or so,
+    // but this is close enough for produce.
+    switch (date.month) {
+      case 1:
+        return Season.winter;
+      case 2:
+        return Season.winter;
+      case 3:
+        return date.day < 21 ? Season.winter : Season.spring;
+      case 4:
+        return Season.spring;
+      case 5:
+        return Season.spring;
+      case 6:
+        return date.day < 21 ? Season.spring : Season.summer;
+      case 7:
+        return Season.summer;
+      case 8:
+        return Season.summer;
+      case 9:
+        return date.day < 22 ? Season.autumn : Season.winter;
+      case 10:
+        return Season.autumn;
+      case 11:
+        return Season.autumn;
+      case 12:
+        return date.day < 22 ? Season.autumn : Season.winter;
+      default:
+        throw ArgumentError('Can\'t return a season for month #${date.month}.');
+    }
+  }
+
+  List<Veggie> get availableVeggies {
+    var currentSeason = _getSeasonForDate(DateTime.now());
+    return veggies.where((v) => v.seasons.contains(currentSeason)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     var dateString = DateFormat('MMMM y').format(DateTime.now());
@@ -25,11 +64,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: <Widget>[
         _buildVeggieList(dateString),
-        // Container(
-        //   color: Colors.green,
-        //   alignment: Alignment.center,
-        //   child: const Text('TODO: My Garden'),
-        // ),
         FavoritesScreen(),
         SearchScreen(),
         SettingScreen(),
@@ -85,8 +119,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             );
-          } else if (index <= veggies.length) {
-            return VeggieCard(veggies[index]);
+          } else if (index <= availableVeggies.length) {
+            return VeggieCard(availableVeggies[index - 1]);
           }
         },
       ),
